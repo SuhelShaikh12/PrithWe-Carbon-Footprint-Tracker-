6a4aad8f94c93046c403bf81f2a036f0bcbe115c
Abhishekcsds <145394772+Abhishekcsds@users.noreply.github.com>
2025-05-01 01:23:41 +0530

    Fifth commit

--- server/db.js
+++ server/db.js
@@ -181,6 +181,7 @@ const { Pool } = pkg;
 const isProduction = process.env.NODE_ENV === 'production';
 
 const pool = new Pool({
+  connectionString: process.env.DB_URI,
   user: process.env.PG_USER,
   host: process.env.PG_HOST,
   database: process.env.PG_DATABASE,
