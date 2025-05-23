a627182b9b0d5bf330d63a3282afa040adb5c9be
Abhishekcsds <145394772+Abhishekcsds@users.noreply.github.com>
2025-05-23 22:42:55 +0530

    Login commit

--- client/vite.config.js
+++ client/vite.config.js
@@ -1,8 +1,29 @@
+// import { defineConfig } from "vite";
+// import react from "@vitejs/plugin-react";
+// //import vitePluginRequire from "vite-plugin-require";
+// //import { defineConfig } from 'vite';
+// //import commonjs from '@rollup/plugin-commonjs';
+
+// // https://vitejs.dev/config/
+// export default defineConfig({
+//   plugins: [react()],
+//   build: {
+//     minify: false,
+//   },
+
+//   server: {
+//     proxy: {
+//       "/api": "http://localhost:3001",
+//     },
+//   },
+//   mimeTypes: {
+//     ".js": "text/plain",
+//   },
+// });
+
+
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
-//import vitePluginRequire from "vite-plugin-require";
-//import { defineConfig } from 'vite';
-//import commonjs from '@rollup/plugin-commonjs';
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -10,13 +31,23 @@ export default defineConfig({
   build: {
     minify: false,
   },
-
   server: {
     proxy: {
-      "/api": "http://localhost:3001",
+      "/api": {
+        target: "http://localhost:3001",
+        changeOrigin: true,
+        secure: false,
+        rewrite: (path) => path.replace(/^\/api/, "/api"),
+        configure: (proxy, options) => {
+          proxy.on("proxyReq", (proxyReq, req, res) => {
+            proxyReq.setHeader("origin", "http://localhost:5173");
+          });
+        }
+      },
     },
-  },
-  mimeTypes: {
-    ".js": "text/plain",
+    cors: {
+      origin: "http://localhost:5173",
+      credentials: true,
+    },
   },
 });
